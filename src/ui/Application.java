db748d5ee07a1b4cf867dc6526af1da3b7ed4db0
Marko Lalic <marko.lalic@gmail.com>
2013-10-29 13:05:05 +0100

    Implement the application main skeleton.
    The application is a simple REPL where commands are created by
    passing the text a user inputs on the command line to a factory
    instance.

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -1,9 +1,37 @@
 package ui;
 
+import commands.Command;
+import commands.CommandFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
-	
-	public static void main(String[] args){
+    private final static String PROMPT = "EchoClient> ";
 
-	}
-
+    public static void main(String[] args) {
+        CommandFactory factory = new CommandFactory();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = null;
+        while (true) {
+            System.out.print(PROMPT);
+            try {
+                line = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            if (line == null) {
+                // EOF
+                return;
+            }
+            Command command = factory.createCommand(line);
+            if (command == null) {
+                System.out.println("Invalid command.");
+                continue;
+            } else {
+                command.execute();
+            }
+        }
+    }
 }
