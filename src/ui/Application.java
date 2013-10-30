d4aa9d5149e0bc29378f5cbc6e3cda57663b48b6
Balsa Bojic <balsha.bojic@gmail.com>
2013-10-30 14:26:38 +0100

    Application with log trace
    In this class we are making new instance of log class, so we can use it
    later in the whole project. When you want to add a log trace in your
    class, you need to make a new Logger instance for that class.
    Example:
    Logger log = Logger.getLogger(Application.class); // with this command
    you are making new logger instance for that class.
    Than you can just send which file are you compiling using log.getName()
    method.
    There are two most important methods that you will need to use:
    log.info(); - for adding some information in log file
    log.error() - for adding information about error in log file
    All loggers made in the project are writing in the same .txt file.

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -7,10 +7,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import logging.Log4JUtils;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
 public class Application {
     private final static String PROMPT = "EchoClient> ";
+    
+    static {
+		DOMConfigurator.configure(Log4JUtils.instance().findLoggerConfigFile());
+		Log4JUtils.instance().prepareLogFile(Logger.getRootLogger());
+	}
 
     public static void main(String[] args) {
+    	
+    	Logger log = Logger.getLogger(Application.class);
+		log.info("Compiling source file: " + log.getName());
+		log.info("Program is started.");
+    	
         CommandFactory factory = new CommandFactory();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String line = null;
@@ -18,8 +33,13 @@ public class Application {
             System.out.print(PROMPT);
             try {
                 line = reader.readLine();
+                
+                log.info("You entered this text: " + line.toString());
             } catch (IOException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                
+                log.error("Exception in the class: " + log.getName());
+                log.error(e.getStackTrace());
             }
             if (line == null) {
                 // EOF
@@ -28,9 +48,13 @@ public class Application {
             Command command = factory.createCommand(line);
             if (command == null) {
                 System.out.println("Invalid command.");
+                
+                log.info("Unfortunately your command is invalid.");
                 continue;
             } else {
                 command.execute();
+                
+                log.info("Entered command has been executed.");
             }
         }
     }
